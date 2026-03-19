aa06d04bd1439eec42e15e208c9a7ed19ba0d700
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-03-19 20:56:55 -0300

    Explicacion de un if y else if

--- 01-introduccion.js
+++ 01-introduccion.js
@@ -1,19 +1,23 @@
 // Variable constante, no se puede redeclarar su valor.
 const number1 = 0;
 // Variable flexible que se puede redeclarar su valor
-let number2 = 0;
-
-number2 = 5;
+const number2 = "0";
 
 const lista = [1, 2, 3, 4, 5];
-
-let i = 0;
 
 /* El ciclo for tiene 3 argumentos:
  * el primer argumento es para incializar el valor del iterador
  * el segundo argumento es para la condicion del ciclo
  * el tercer argumento es la cantidad que se ira sumando el iterador.
 */
-for (let i = 0; i < lista.length; i++) {
-
+for (let i = 0; i <= lista.length; i++) {
+//   console.log(`Valor del iterador ${i}`);
 }
+
+if (number1 === number2) {
+  console.log(`El valor del number 1 es igual al del number 2`)
+}
+/* Diferencias entre if y else if:
+ * El if siempre va a preguntar la condicion
+ * el else if solo te lo va a validar una vez solo si se cumple la condicion.
+*/
