a01ea368a14c7a359a3b483a3a3a246eb9f06092
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-03-19 20:34:00 -0300

    Mi primer commit

---
+++ 01-introduccion.js
@@ -0,0 +1,8 @@
+// Variable constante, no se puede redeclarar su valor.
+const number1 = 0;
+// Variable flexible que se puede redeclarar su valor
+let number2 = 0;
+
+number2 = 5;
+
+console.log(number2)
